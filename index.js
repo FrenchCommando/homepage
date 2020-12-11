6dc0afd724ddbd356b7f517c088ba09386f9cee7
FrenchCommando <martialrengmail.com>
2020-12-11 10:58:46 -0500

    index add js code

---
+++ index.js
@@ -0,0 +1,9 @@
+var myArr = ["Audi", "BMW", "Ford", "Honda", "Jaguar", "Nissan", "Tesla"];
+var temp, item, a, i;
+temp = document.getElementsByTagName("template")[0];
+item = temp.content.querySelector("div");
+for (i = 0; i < myArr.length; i++) {
+  a = document.importNode(item, true);
+  a.textContent += myArr[i];
+  document.body.appendChild(a);
+}
